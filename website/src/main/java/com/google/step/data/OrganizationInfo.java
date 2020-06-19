e3391090ce0392086623cce33662d4073e1bc868
hajiler <hajiler@me.com>
2020-06-19 17:18:21 +0000

    Implements comparison for duplicates, which still needs to be worked on. As well as validate function, which makes optional fields null if needed.

--- website/src/main/java/com/google/step/data/OrganizationInfo.java
+++ website/src/main/java/com/google/step/data/OrganizationInfo.java
@@ -2,7 +2,10 @@ package com.google.sps.data;
 
 import com.google.appengine.api.datastore.Entity;
 import com.google.appengine.api.datastore.Query;
+import java.util.Objects;
 import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import org.apache.commons.validator.UrlValidator;
 
 public final class OrganizationInfo {
   private final Entity entity;
@@ -17,8 +20,35 @@ public final class OrganizationInfo {
     newOrganization.setProperty("webLink", request.getParameter("webLink"));
     newOrganization.setProperty("donateLink", request.getParameter("donateLink"));
     newOrganization.setProperty("about", request.getParameter("about"));
+    newOrganization.setProperty("flags", new ArrayList<String>());
 
     return newOrganization;
+  }
+
+  public Boolean isValid() {
+    UrlValidator urlValidator = new UrlValidator();
+    //Required fields
+    if (((String) this.entity.getProperty("name")).isEmpty() || 
+    	  ((String) this.entity.getProperty("name")).isEmpty() ||
+        !urlValidator.isValid(((String) this.entity.getProperty("webLink")))) {
+      return false;
+    }
+    //Change invalid Optional Fields to null
+    if (urlValidator.isValid(((String) this.entity.getProperty("donateLink")))) {
+      this.entity.setProperty("donateLink",null);
+    }
+    return true;
+  }
+
+  public void compare(Entity duplicate) {
+    this.entity.getProperties().forEach((property, value) -> {
+      if (Objects.isNull(value) && !Objects.isNull(duplicate.getProperty(property))) {
+        this.entity.setProperty(property, duplicate.getProperty(property));
+      } 
+      else if (!Objects.isNull(value) && !Objects.isNull(duplicate.getProperty(property))) {
+        //TODO: Handle case where fields are conflicting
+      }
+    });
   }
 
   public Entity getEntity() {
